4d0e6c2e36fb0c6d56cd23ff707175a745c7554e
hyunse0 <78924207+hyunse0@users.noreply.github.com>
2021-11-19 01:50:49 +0900

    Add change password

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Signup from '../views/accounts/Signup.vue'
 import Login from '../views/accounts/Login.vue'
+import Password from '../views/accounts/Password.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/accounts/login',
     name: 'Login',
     component: Login,
+  },
+  {
+    path: '/accounts/password',
+    name: 'Password',
+    component: Password,
   },
 ]
 
---
+++ client/src/views/accounts/Password.vue
@@ -0,0 +1,80 @@
+<template>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <v-card class="elevation-12">
+          <v-toolbar color="primary" dark flat>
+            <v-toolbar-title>Password Change Form</v-toolbar-title>
+          </v-toolbar>
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                id="old_password"
+                label="기존 비밀번호"
+                name="old_password"
+                prepend-icon="mdi-lock"
+                type="password"
+                v-model="changepasswordData.old_password"
+                @keypress.enter="changepassword(changepasswordData)"
+                :rules="passwordRules"
+              ></v-text-field>
+
+              <v-text-field
+                id="new_password1"
+                label="새 비밀번호"
+                name="new_password1"
+                prepend-icon="mdi-lock"
+                type="password"
+                v-model="changepasswordData.new_password1"
+                @keypress.enter="changepassword(changepasswordData)"
+                :rules="passwordRules"
+              ></v-text-field>
+
+              <v-text-field
+                id="new_password2"
+                label="새 비밀번호 확인"
+                name="new_password2"
+                prepend-icon="mdi-lock"
+                type="password"
+                v-model="changepasswordData.new_password2"
+                @keypress.enter="changepassword(changepasswordData)"
+                :rules="passwordRules"
+              ></v-text-field>
+            </v-form>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="primary" @click="changepassword(changepasswordData)">Submit</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+export default {
+  name: "changepassword",
+  data() {
+    return {
+      changepasswordData: {
+        new_password1: null,
+        new_password2: null,
+        old_password: null
+      },
+      passwordRules: [
+        value => !!value || "필수 정보입니다.",
+        value =>
+          (value || "").length >= 8 || "비밀번호는 8글자 이상이어야 합니다."
+      ]
+    };
+  },
+  methods: {
+    ...mapActions("accounts", ["changepassword"])
+  }
+};
+</script>
+
+<style>
+</style>
