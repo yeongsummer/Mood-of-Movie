dfc48ce07bbabe6775f1463c44f94028a2ac5128
summer <qwer62636275@gmail.com>
2021-11-18 17:24:56 +0900

    No

--- server/movies/urls.py
+++ server/movies/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.movie_list_create),
-    path('<int:movie_pk>/', views.movie_detail_update_delete),
+    path('<int:movie_pk>/', views.movie_detail),
     path('review/', views.review_detail_update_delete),
     path('review/<int:review_pk>/', views.review_detail_update_delete),
     path('comment/', views.comment_list_create),
