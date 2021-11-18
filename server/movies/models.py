057964f20a56080872fec101f9484303d546fbc0
summer <qwer62636275@gmail.com>
2021-11-18 09:28:34 +0900

    no

--- server/movies/models.py
+++ server/movies/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Director(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    overview = models.TextField()
+    director = models.CharField(max_length=50)
+    release_date = models.DateField()
+    poster_path = models.TextField()
+    tag = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    genres = models.ManyToManyField(Genre)
+    like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies')
+    actor = models.ForeignKey(Actor)
+    director = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    rank = models.IntegerField(default=5)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.content
+
+
--- server/movies/views.py
+++ server/movies/views.py
@@ -1,3 +1,88 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, get_list_or_404
+from django.views.decorators.http import require_safe, require_POST
+from django.http import JsonResponse
+from collections import defaultdict
 
-# Create your views here.
+from .models import Movie
+
+
+def movie_index(request):
+    movies = Movie.objects.all()
+    context = {
+        'movies': movies,
+    }
+    return render(request, 'movies/index.html', context)
+
+
+def movie_detail(request, movie_pk):
+    movie = get_object_or_404(Movie, pk=movie_pk)
+    context = {
+        'movie': movie,
+    }
+    return render(request, 'movies/detail.html', context)
+
+
+def movie_create(request, movie_pk):
+    pass
+
+
+def movie_update(request, movie_pk):
+    pass
+
+
+def movie_delete(request, movie_pk):
+    pass
+
+
+def movie_like(request, movie_pk):
+    if request.user.is_authenticated:
+        movie = get_object_or_404(Movie, pk=movie_pk)
+        user = request.user
+
+        if movie.like_users.filter(pk=user.pk).exists():
+            movie.like_users.remove(user)
+            is_like = False
+        else:
+            movie.like_users.add(user)
+            is_like = True
+        context = {
+            'is_like': is_like,
+            'like_count': movie.like_users.count()
+        }
+        return JsonResponse(context)
+        
+        # return redirect('community:index')
+    return redirect('accounts:login')
+
+
+@require_safe
+def review_detail(request):
+    pass
+
+
+def review_create(request):
+    pass
+
+
+def review_update(request):
+    pass
+
+
+def review_delete(request):
+    pass
+
+
+def review_like(request):
+    pass
+
+
+def comment_create(request):
+    pass
+
+
+def comment_delete(request):
+    pass
+
+
+def follow(request):
+    pass
